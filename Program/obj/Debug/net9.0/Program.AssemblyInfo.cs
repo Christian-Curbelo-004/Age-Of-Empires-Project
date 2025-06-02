6d553b12a38429cf063dc2822d9ea18ccdaf7339
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-06-02 19:00:10 -0300

    anda a saber

--- Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935054019d129d2658aa92041e7b3947b492c346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4145c6221faebd46fc16327980ea08033610767b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
