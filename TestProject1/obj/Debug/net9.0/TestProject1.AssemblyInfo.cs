13434b7da15187eb133a0b367507e3ce1496fa27
chris <chris94curbelo@gmail.com>
2025-06-02 23:40:41 -0300

    subiendo interfaz fachada, clases player 1 y 2

--- TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+185c550a15953e0ccdddbbce349237b424ffd0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ac06046dc570cb5e39768bb0aa337741a95037")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
