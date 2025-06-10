92a91cb26243293d9eb940b9a5a4af230a7d5edf
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-06-10 13:16:49 -0300

    armando diagramas

--- ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cba15caaa70b537265e96c9fc0aaa431d52aba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84780089bea4aa879d1154bab683ed41da1efa82")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
