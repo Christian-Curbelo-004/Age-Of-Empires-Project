2ff9c6dbd879b85efd56bad93b8253e69d89eddc
joaquinbauza <joaquin.bauza@correo.ucu.edu.uy>
2025-06-15 13:12:44 -0300

    agregando cosas a clase aldeano

--- ClassLibrary1/CivilizationDirectory/Villagers.cs
+++ ClassLibrary1/CivilizationDirectory/Villagers.cs
@@ -9,11 +9,18 @@ public class Villagers : ICharacter
     public int Life { get; set; }
     public int AttackValue { get; set; }
     public int ValueCell { get; set; }
+    public Dictionary<string, int> Resources { get; set;}
+    
     public Villagers(int life, int attackValue, bool building)
         {
         Life = life;
         AttackValue = attackValue;
         ValueCell = 0;
+
+        Resources = new Dictionary<string, int>
+        {
+            { "Stone", 0 }, { "Gold", 0 }, { "Wood", 0 }
+        };
         }
 
     public int Attack(ICharacter target)  //cambie el void que devolvia por un int, ya que nos interesa unicamente el valor, el console.writeline va en el program
@@ -30,5 +37,9 @@ public class Villagers : ICharacter
         return Life;
         //Console.WriteLine($"El aldeano recibio {damage} de daño");
     }
-    
+
+    public Dictionary<string, int> ShowResources()
+    {
+        return Resources;
+    }
 }
--- Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2812558450e20fef60c9e0341e0c4817655cba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba1d6989e973288e10803555db4a8c8192d274d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
