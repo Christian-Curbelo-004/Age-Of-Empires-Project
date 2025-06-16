6ddc96dca71bc82440253ae9f945346358e24aab
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-06-16 20:41:43 -0300

    Colocar en celda

--- ClassLibrary1/MapDirectory/Map.cs
+++ ClassLibrary1/MapDirectory/Map.cs
@@ -1,29 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
+
 namespace ClassLibrary1;
 
 public class Map
 {
     private int _height;
     private int _length;
-    public Cell[,] map;
-    
+    public Cell[,] map; 
+
     private const int MinDimension = 0;
     private const int MaxDimension = 100;
+
     public Map(int height, int length)
     {
-        _height= Math.Max(MinDimension, Math.Min(MaxDimension, height));
-        _length= Math.Max(MinDimension, Math.Min(MaxDimension, length));
-        this.map = new Cell[length, height];
-        if (map != null )
+        _height = Math.Max(MinDimension, Math.Min(MaxDimension, height));
+        _length = Math.Max(MinDimension, Math.Min(MaxDimension, length));
+        this.map = new Cell[_length, _height];
+        
+        for (int i = 0; i < _length; i++)
         {
-            for (int i = 0; i < Math.Min(length, map.GetLength(dimension: 0)); i++)
+            for (int j = 0; j < _height; j++)
             {
-                for (int x = 0; x < Math.Min(height, map.GetLength(dimension: 1)); x++)
-                {
-                    this.map[i,x] = new Cell(i,x);
-                }
+                this.map[i, j] = new Cell(i, j);
             }
+        }
+    }
+    public bool IsWithinBounds(int x, int y)
+    {
+        return x >= 0 && x < _length && y >= 0 && y < _height;
+    }
+    
+    public bool IsCellOccupied(int x, int y)
+    {
+        if (IsWithinBounds(x, y))
+        {
+            return map[x, y].IsOccupied;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException($"La posición ({x}, {y}) está fuera de los límites del mapa.");
+        }
+    }
+    public void OccupyCell(int x, int y)
+    {
+        if (IsWithinBounds(x, y))
+        {
+            map[x, y].IsOccupied = true;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException($"La posición ({x}, {y}) está fuera de los límites del mapa.");
         }
     }
 }
