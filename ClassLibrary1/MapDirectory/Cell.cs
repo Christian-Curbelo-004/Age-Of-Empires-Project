4604edb0a51a4e7929ee7275b3ee4b6a148918ea
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-06-16 20:16:45 -0300

    Cell acmododado

--- ClassLibrary1/MapDirectory/Cell.cs
+++ ClassLibrary1/MapDirectory/Cell.cs
@@ -2,19 +2,14 @@
 
 public class Cell
 {
-    public int PosX { get; set;}
-    public int PosY { get; set;}
+    public int PosX { get; set; }
+    public int PosY { get; set; }
+    public bool IsOccupied { get; set; }
 
     public Cell(int x, int y)
     {
         PosX = x;
         PosY = y;
+        IsOccupied = false; 
     }
-}
-public class EvaluateCell
-{
-}
-
-public class PrintCell
-{
 }
