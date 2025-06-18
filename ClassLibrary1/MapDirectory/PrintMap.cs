d498fc03410d105f4e753b9cf36b5aa442245a3e
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-06-18 16:16:19 -0300

    PrintMap arreglado

--- ClassLibrary1/MapDirectory/PrintMap.cs
+++ ClassLibrary1/MapDirectory/PrintMap.cs
@@ -21,31 +21,33 @@ namespace ClassLibrary1
 
             int height = _map.map.GetLength(1);
             int length = _map.map.GetLength(0);
+            
+            int coordWidth = Math.Max(height, length).ToString().Length;
+            string numFormat = new string('0', coordWidth);
 
             var result = new StringBuilder();
             
-            result.Append("     ");
+            result.Append(' ', coordWidth + 1);
             for (int j = 0; j < length; j++)
             {
-                result.Append($"{j % 10} ");
+                result.Append(j.ToString(numFormat) + " ");
             }
             result.AppendLine();
 
             for (int i = 0; i < height; i++)
             {
-                result.Append($" {i % 10}  "); 
+                result.Append(i.ToString(numFormat) + " ");
                 for (int j = 0; j < length; j++)
                 {
                     Cell cell = _map.map[j, i];
-                    result.Append($"{cell} "); 
+                    result.Append($"{cell.ToString().PadRight(coordWidth)} ");
                 }
-                result.AppendLine($" {i % 10}"); 
+                result.AppendLine(i.ToString(numFormat));
             }
-            
-            result.Append("     "); 
+            result.Append(' ', coordWidth + 1);
             for (int j = 0; j < length; j++)
             {
-                result.Append($"{j % 10} ");
+                result.Append(j.ToString(numFormat) + " ");
             }
             result.AppendLine();
 
