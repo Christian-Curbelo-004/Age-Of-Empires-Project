7b428309d178ef5260f9f8277a434b4c3bc28718
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-06-18 16:46:41 -0300

    subiendo test chivarlycenter

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,18 +1,25 @@
-﻿using NUnit.Framework;
-
+﻿using System.Collections.Generic;
+using InfanteryCenter;  
+using NUnit.Framework; 
 
 namespace TestProject1;
 
-public class Tests
+public class TestsInfanteyCenter
 {
+    private ChivarlyCenter.ChivarlyCenter center;
     [SetUp]
     public void Setup()
     {
+        center = new ChivarlyCenter.ChivarlyCenter(20, 10, "ChivarlyCenter", 100);
     }
 
     [Test]
-    public void Test1()
+    public void ConstructionCost()
     {
-        Assert.Pass();
+        
+        var constructionCost = new Dictionary<string, int>();
+        center.GetconstructionCost(constructionCost);
+        Assert.AreEqual(100, constructionCost["Madera"]);
+        Assert.AreEqual(20, constructionCost["Oro"]);
     }
 }
