368d8bed7fd08ca5dcda2a4424759aef457e94a6
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-06-18 16:50:11 -0300

    actualizando

--- ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da358685f48f3b18936979c1ee279603e885d60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3707417742f3690323dab0765f02d8d3bf64bb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
