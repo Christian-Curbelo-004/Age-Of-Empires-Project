bc75d73111a589d4d70d5d5e4f3492b03de2443e
joaquinbauza <joaquin.bauza@correo.ucu.edu.uy>
2025-06-21 15:23:52 -0300

    agregando cosas a program

--- ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe418391bc8e0cd13a9e9d056a55d5412452fd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8d2fc267786ffa515ca5598cc6852bbfdb51c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
