4c05df24e94736346e8f88c5d54594313230d53f
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-06-23 20:25:52 -0300

    actualizando

--- ClassLibrary1/CivilizationDirectory/Villagers.cs
+++ ClassLibrary1/CivilizationDirectory/Villagers.cs
@@ -1,9 +1,4 @@
 ﻿using ClassLibrary1.CivilizationDirectory;
-using ClassLibrary1.DepositDirectory;
-using ClassLibrary1.FacadeDirectory;
-using ClassLibrary1.LogicDirectory;
-using CreateBuildings;
-using GameResourceType = GameModels.GameResourceType;
 namespace ClassLibrary1;
 
 public class Villagers : ICharacter
@@ -11,18 +6,15 @@ public class Villagers : ICharacter
     public int Life { get; set; }
     public int AttackValue { get; set; }
     public int Speed { get; set; }
-    public IWorkTarget CurrentTask { get; set; }
     public int OwnerId { get; set; }
-    public string Name { get; set; }
-    public string EntityType => "Villagers";
     public bool IsFree { get; set; } = true;
-    public Dictionary<GameResourceType, int> ConstructionCost { get;  set; } = new ();
     
-    public Villagers(int life, int attackValue, int ownerId)
+    public Villagers(int life, int attackValue, int ownerId, int speed)
     {
         OwnerId= ownerId;
         Life = life;
         AttackValue = attackValue;
+        Speed = speed;
     }
     public int Attack(ICharacter target) //cambie el void que devolvia por un int, ya que nos interesa unicamente el valor, el console.writeline va en el program
     {
@@ -31,20 +23,10 @@ public class Villagers : ICharacter
 
         return DamageDone;
     }
-
     public int RecieveAttack(int damage)
     {
         Life -= damage;
         return Life;
         //Console.WriteLine($"El aldeano recibio {damage} de daño");
-    }
-    public Dictionary<GameResourceType, int> GetCreationCost()
-    {
-        return new Dictionary<GameResourceType, int>
-        {
-            {GameResourceType.Stone,100},
-            {GameResourceType.Gold, 40},
-            {GameResourceType.Food, 150}
-        };
     }
 }
