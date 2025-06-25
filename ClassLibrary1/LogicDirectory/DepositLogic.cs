31debf10009dec1d4f12ea9afc265e68ef82de8a
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-06-24 21:30:21 -0300

    actualizando

---
+++ ClassLibrary1/LogicDirectory/DepositLogic.cs
@@ -0,0 +1,6 @@
+using ClassLibrary1.QuaryDirectory;
+namespace ClassLibrary1.LogicDirectory;
+public class DepositLogic
+{
+    
+}
---
+++ ClassLibrary1/QuaryDirectory/GetResources.cs
@@ -0,0 +1,9 @@
+namespace ClassLibrary1.QuaryDirectory;
+
+public class GetResources
+{
+    public int Collectors { get; set; } = 1;
+    public void GetResourcess()
+    {
+    }
+}
