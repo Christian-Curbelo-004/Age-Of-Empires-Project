3a6a5f4a404217700b2f3c3012cc7a343e8c1836
Christian Curbelo <chris94curbelo@gmail.com>
2025-06-28 11:51:34 -0300

    sacando speed de buildings y imapentity

--- ClassLibrary1/BuildingsDirectory/Buildings.cs
+++ ClassLibrary1/BuildingsDirectory/Buildings.cs
@@ -8,7 +8,7 @@ namespace CreateBuildings
         public int OwnerId { get; set; }
         public (int X, int Y) Position { get; set; }
         public string Name { get; set; }
-        public int Speed { get; } = 0; // Buildings typically do not move, so speed is set to 0
+       // public int Speed { get; }  // Buildings typically do not move, so speed is set to 0
         public int Endurence { get; set; }
         public Buildings(int endurence, string name)
         {
@@ -16,7 +16,7 @@ namespace CreateBuildings
             Name = name;
             OwnerId = OwnerId;
             Position = (0, 0); // Default position, can be changed later
-            Speed = 0;
+            
             //  Capacity = capacity;
         }
 
--- ClassLibrary1/MapDirectory/iMapEntidad.cs
+++ ClassLibrary1/MapDirectory/iMapEntidad.cs
@@ -5,7 +5,7 @@ namespace ClassLibrary1.MapDirectory
         string Name { get; set; }
         int OwnerId { get; set; }
         (int X, int Y) Position { get; set; }
-        int Speed { get; } // Si no se mueve, poner Speed = 0 en la clase concreta
+      //  int Speed { get; } // Si no se mueve, poner Speed = 0 en la clase concreta
     }
 
     public interface IResource : IMapEntity
