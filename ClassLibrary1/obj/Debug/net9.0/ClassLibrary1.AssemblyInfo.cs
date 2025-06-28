27c751bbd4a2f00d68fca5ac0174075d1d545b39
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-06-28 10:26:27 -0300

    program sin token

--- ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05268fd31b4f4a9c0311d7fd70e5f894a9ad7248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ec6c5568b6d730238e028efd701d9c617abaec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
