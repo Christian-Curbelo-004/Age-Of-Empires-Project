edaa0d6c5192a0a9d789716099a159faccaa94a1
Christian Curbelo <chris94curbelo@gmail.com>
2025-06-28 11:58:24 -0300

    sacando speed de buildings x2

--- ClassLibrary1/BuildingsDirectory/CivicCenter.cs
+++ ClassLibrary1/BuildingsDirectory/CivicCenter.cs
@@ -6,7 +6,7 @@ namespace ClassLibrary1.BuildingsDirectory;
     public class CivicCenter : Buildings , IMapEntity , ICapacity
     {
         public int OwnerId { get; set; }
-        public int Speed { get; } = 0; // El Civic Center no se mueve, por lo que su velocidad es 0
+      //  public int Speed { get; } = 0; // El Civic Center no se mueve, por lo que su velocidad es 0
         public (int X, int Y) Position { get; set; }
         
         public const int MaxHealth = 500;
