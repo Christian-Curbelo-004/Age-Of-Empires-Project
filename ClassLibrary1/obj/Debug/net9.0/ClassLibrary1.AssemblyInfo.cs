14fd2ba9ff705f5a9e5983142cab6cef93902a4e
Christian Curbelo <chris94curbelo@gmail.com>
2025-07-05 21:36:45 -0300

    subiendo test

--- ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d70357630105b03def12814b124d78d6005f653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ce635e2e713143b7d287ee315f8211bca83229")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TestProject/InfanteryCenter.cs
@@ -0,0 +1,27 @@
+
+using ClassLibrary1.BuildingsDirectory;
+
+
+namespace TestProject
+{
+    public class TestInfanteryCenter
+    {
+        private InfanteryCenter _centerInf;
+
+        [Test]
+        public void InfanteryCenterTest()
+        {
+            int expectedendurence = 20;
+            int expectedConstructionTimeleft = 10;
+            string expectedname = "InfanteryCenter";
+            int expectedOwnerId = 1223;
+
+            _centerInf = new InfanteryCenter(20, 10, "InfanteryCenter", 1223);
+
+            Assert.That(_centerInf.Name, Is.EqualTo(expectedname));
+            Assert.That(_centerInf.ConstructionTime, Is.EqualTo(expectedConstructionTimeleft));
+            Assert.That(_centerInf.Endurence, Is.EqualTo(expectedendurence));
+            Assert.That(_centerInf.OwnerId, Is.EqualTo(expectedOwnerId));
+        }
+    }
+}
